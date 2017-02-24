39d7ce57ac62a6f31a9fc10d4f32d5b3bf0f3de0
andrew <andvoz@mail.ru>
2017-02-24 10:02:00 +0400

    study.js

---
+++ src/MyMemory.MVC/JS/Study/study.js
@@ -0,0 +1,32 @@
+﻿function StudyTest(urlStartStudy, urlNextStep) {
+    var _urlStartStudy = urlStartStudy;
+    var _urlNextStep = urlNextStep;
+
+    function StartStudy() {
+        $.ajax({ type: "POST", url: _urlStartStudy, data: { groupId: 0},
+            success: function (response) {
+                console.log(response);
+                $("#txtQuestion").text(response.Question.Text);
+            }
+        });
+    }
+
+    function NextStep() {
+        $.ajax({ type: "POST", url: _urlNextStep, data: { answer: $("#inpAnswer").val() },
+            success: function (response) {
+                console.log(response);
+                $("#txtQuestion").text(response.Question.Text);
+            }
+        });
+    }
+
+    this.Start = function()
+    {
+        StartStudy();
+    }
+
+    $("#btnSubmit").click(function () {
+         NextStep();
+    });
+
+}
