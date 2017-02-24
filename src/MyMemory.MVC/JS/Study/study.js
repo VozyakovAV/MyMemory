30f12bcd73a5afb3f1d779d273d8b2324f79c0a1
andrew <andvoz@mail.ru>
2017-02-24 10:05:11 +0400

    study.js

--- src/MyMemory.MVC/JS/Study/study.js
+++ src/MyMemory.MVC/JS/Study/study.js
@@ -1,32 +1,36 @@
 ﻿function StudyTest(urlStartStudy, urlNextStep) {
     var _urlStartStudy = urlStartStudy;
     var _urlNextStep = urlNextStep;
+    var _txtQuestion = $("#txtQuestion");
+    var _inpAnswer = $("#inpAnswer");
+    var _btnSubmit = $("#btnSubmit");
 
     function StartStudy() {
-        $.ajax({ type: "POST", url: _urlStartStudy, data: { groupId: 0},
+        $.ajax({
+            type: "POST", url: _urlStartStudy, data: { groupId: 0 },
             success: function (response) {
                 console.log(response);
-                $("#txtQuestion").text(response.Question.Text);
+                _txtQuestion.text(response.Question.Text);
             }
         });
     }
 
     function NextStep() {
-        $.ajax({ type: "POST", url: _urlNextStep, data: { answer: $("#inpAnswer").val() },
+        $.ajax({
+            type: "POST", url: _urlNextStep, data: { answer: _inpAnswer.val() },
             success: function (response) {
                 console.log(response);
-                $("#txtQuestion").text(response.Question.Text);
+                _txtQuestion.text(response.Question.Text);
             }
         });
     }
 
-    this.Start = function()
-    {
+    this.Start = function () {
         StartStudy();
     }
 
-    $("#btnSubmit").click(function () {
-         NextStep();
+    _btnSubmit.click(function () {
+        NextStep();
     });
 
 }
