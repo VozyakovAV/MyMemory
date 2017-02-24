0a25167866560adde3e28d96bef315e49b2a8890
andrew <andvoz@mail.ru>
2017-02-24 10:41:17 +0400

    study.js

--- src/MyMemory.MVC/JS/Study/study.js
+++ src/MyMemory.MVC/JS/Study/study.js
@@ -3,14 +3,17 @@
     var _urlNextStep = urlNextStep;
     var _txtQuestion = $("#txtQuestion");
     var _inpAnswer = $("#inpAnswer");
+    var _inpCorrectAnswer = $("#inpCorrectAnswer");
+    var _txtMessage = $("#txtMessage");
     var _btnSubmit = $("#btnSubmit");
+    var _groupId = 0;
 
     function StartStudy() {
         $.ajax({
-            type: "POST", url: _urlStartStudy, data: { groupId: 0 },
+            type: "POST", url: _urlStartStudy, data: { groupId: _groupId },
             success: function (response) {
                 console.log(response);
-                _txtQuestion.text(response.Question.Text);
+                ShowQuestion(response.Question.Text);
             }
         });
     }
@@ -20,10 +23,41 @@
             type: "POST", url: _urlNextStep, data: { answer: _inpAnswer.val() },
             success: function (response) {
                 console.log(response);
-                _txtQuestion.text(response.Question.Text);
+                ShowResult(response);
+                setTimeout(function () {
+                    ShowQuestion(response.Question.Text);
+                }, 2000)
             }
         });
     }
+
+    function ShowQuestion(question) {
+        _inpCorrectAnswer.hide();
+        _inpAnswer.val("");
+        _txtMessage.html("");
+        _txtQuestion.text(question);
+    }
+
+    function ShowResult(response) {
+        if (response.PrevAnswer != null) {
+            if (response.PrevAnswer.IsCorrectAnswer) {
+                ShowSuccessResult();
+            }
+            else {
+                ShowErrorResult(response.PrevAnswer.CorrectAnswer)
+            }
+        }
+    }
+
+    function ShowSuccessResult() {
+        _txtMessage.html("Верно!").css("color", "green");
+    };
+    function ShowErrorResult(correctAnswer) {
+        _txtMessage.html("Упс!").css("color", "red");
+        _inpAnswer.addClass("")
+        _inpCorrectAnswer.show();
+        _inpCorrectAnswer.val(correctAnswer);
+    };
 
     this.Start = function () {
         StartStudy();
