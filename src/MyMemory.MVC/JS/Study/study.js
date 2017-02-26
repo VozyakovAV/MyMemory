59ddeafedefb6da2b7f9e450ff10c7dd9fa6b131
andrew <andvoz@mail.ru>
2017-02-26 13:36:11 +0400

    study.js

--- src/MyMemory.MVC/JS/Study/study.js
+++ src/MyMemory.MVC/JS/Study/study.js
@@ -12,10 +12,7 @@
         $.ajax({
             type: "POST", url: _urlStartStudy, data: { groupId: _groupId },
             success: function (response) {
-                var next = ShowResult(response);
-                if (next) {
-                    ShowQuestion(response);
-                }
+                ShowResult(response);
             }
         });
     }
@@ -24,12 +21,7 @@
         $.ajax({
             type: "POST", url: _urlNextStep, data: { answer: _inpAnswer.val() },
             success: function (response) {
-                var next = ShowResult(response);
-                if (next) {
-                    setTimeout(function () {
-                        ShowQuestion(response);
-                    }, 2000)
-                }
+                ShowResult(response);
             }
         });
     }
@@ -38,58 +30,30 @@
         console.log(response);
 
         if (response.Message != null) {
-            _txtMessage.html(response.Message).css("color", "black");
-            _btnSubmit.hide();
-            _inpAnswer.hide();
-            _inpCorrectAnswer.hide();
-            return false;
+            ShowViewMessage(response);
         }
-
-        if (response.PrevAnswer != null) {
+        else if (response.PrevAnswer != null) {
             if (response.PrevAnswer.IsCorrectAnswer) {
-                ShowSuccessResult();
-                return true;
+                ShowViewCorrectAnswer(response);
             }
             else {
-                ShowErrorResult(response.PrevAnswer.CorrectAnswer)
-                return false;
+                ShowViewIncorrectAnswer(response);
             }
         }
-
-        return true;
-    }
-
-    function ShowQuestion(response) {
-        _inpCorrectAnswer.hide();
-        _inpAnswer.val("");
-        _txtMessage.html("");
-        if (response.Message == null) {
-            _txtQuestion.text(response.Question.Text);
-            _inpAnswer.focus();
-            _inpAnswer.keyup(function (e) {
-                if (e.keyCode == 13) {
-                    NextStep();
-                }
-            });
-        }
         else {
-            _txtMessage.html(response.Message).css("color", "black");
-            _btnSubmit.hide();
+            ShowViewQuestion(response);
         }
     }
-
-    function ShowSuccessResult() {
-        _txtMessage.html("Верно!").css("color", "green");
-    };
-    function ShowErrorResult(correctAnswer) {
-        _txtMessage.html("Упс!").css("color", "red");
-        _inpAnswer.addClass("")
-        _inpCorrectAnswer.show();
-        _inpCorrectAnswer.val(correctAnswer);
-    };
 
     this.Start = function () {
-        _inpCorrectAnswer.prop("disabled", true);
+        //ShowViewQuestion();
+        //_inpCorrectAnswer.prop("disabled", true);
+        _inpAnswer.keyup(function (e) {
+            if (e.keyCode == 13) {
+                _btnSubmit.click();
+            }
+        });
+
         StartStudy();
     }
 
@@ -97,4 +61,45 @@
         NextStep();
     });
 
+    function ShowViewMessage(responce) {
+        _txtQuestion.hide();
+        _inpCorrectAnswer.hide();
+        _inpAnswer.hide();
+        _btnSubmit.hide();
+        _txtMessage.html(response.Message).css("color", "black").show();
+    }
+
+    function ShowViewQuestion(response) {
+        _txtQuestion.text(response.Question.Text);
+        _inpCorrectAnswer.hide();
+        _txtMessage.hide();
+        _inpAnswer.prop("disabled", false).val("").focus();
+        _btnSubmit.prop("disabled", false).html("Проверить").unbind().click(function () {
+            NextStep();
+        });
+    }
+
+    function ShowViewCorrectAnswer(response) {
+        _txtQuestion.prop("disabled", true);
+        _inpCorrectAnswer.hide();
+        _txtMessage.html("Верно!").css("color", "green").show();
+        _inpAnswer.prop("disabled", true);
+        //_inpAnswer.parent().addClass("has-success");
+        _btnSubmit.prop("disabled", true);
+        /*_btnSubmit.html("Дальше").unbind().click(function () {
+            ShowViewQuestion(response);
+        });*/
+        setTimeout(function () {
+            ShowViewQuestion(response);
+        }, 2000)
+    }
+
+    function ShowViewIncorrectAnswer(response) {
+        _inpAnswer.prop("disabled", true);
+        _inpCorrectAnswer.prop("disabled", true).val(response.PrevAnswer.CorrectAnswer).show();
+        _txtMessage.html("Упс!").css("color", "red").show();
+        _btnSubmit.prop("disabled", false).html("Дальше").unbind().click(function () {
+            ShowViewQuestion(response);
+        });
+    }
 }
