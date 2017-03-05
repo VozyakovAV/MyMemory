946a815f54c6c6d9ee89ff0cf7f001d0f6ed4347
andrew <andvoz@mail.ru>
2017-03-05 09:53:48 +0400

    taskManager

---
+++ src/MyMemory.BLL/Managers/StudyManager/ItemManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyMemory.Domain;
+
+namespace MyMemory.BLL
+{
+    public interface IItemManager<T> where T:class
+    {
+        T GetNextItem();
+        void WriteAnswer(T item, bool isCorrect);
+    }
+
+    public class ItemManager : IItemManager<MemoryTask>
+    {
+        private readonly UnitOfWork _uow;
+        private readonly TaskRepository _taskRepository;
+        private readonly StepsStudyRepository _stepsStudyRepository;
+        private readonly MemoryManager _mng;
+
+        public ItemManager()
+        {
+            this._uow = new UnitOfWork();
+            this._taskRepository = new TaskRepository(_uow);
+            this._stepsStudyRepository = new StepsStudyRepository(_uow);
+            this._mng = new MemoryManager();
+        }
+
+        public MemoryTask GetNextItem()
+        {
+            var tasks = _taskRepository.GetItems()
+                .Where(x => x.Deadline <= DateTime.Now)
+                .OrderByDescending(x => x.StepNumber);
+
+            return tasks.FirstOrDefault();
+        }
+
+        public void WriteAnswer(MemoryTask task, bool isCorrect)
+        {
+            var steps = GetSteps();
+            var currentStep = steps.Single(x => x.Number == task.StepNumber);
+
+            MemoryStepsStudy nextStep = isCorrect
+                ? steps.FirstOrDefault(x => x.Number == currentStep.Number + 1)
+                : steps.FirstOrDefault(x => x.Number == currentStep.Number - 1);
+
+            if (nextStep == null)
+                nextStep = currentStep;
+
+            task.StepNumber = nextStep.Number;
+            task.Deadline = AddPeriod(DateTime.Now, nextStep);
+
+            _mng.SaveTask(task);
+        }
+
+        private MemoryStepsStudy[] GetSteps()
+        {
+            return _stepsStudyRepository.GetItems().OrderBy(x => x.Number).ToArray();
+        }
+
+        private DateTime AddPeriod(DateTime date, MemoryStepsStudy step)
+        {
+            switch (step.Format)
+            {
+                case PeriodFormat.Min:      return date.AddMinutes(step.Period);
+                case PeriodFormat.Hour:     return date.AddHours(step.Period);
+                case PeriodFormat.Day:      return date.AddDays(step.Period);
+                case PeriodFormat.Month:    return date.AddMonths(step.Period);
+                case PeriodFormat.Year:     return date.AddYears(step.Period);
+            }
+
+            return date;
+        }
+    }
+}
---
+++ src/MyMemory.Tests/Study/TestStudy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMemory.Domain;
+using MyMemory.BLL;
+
+namespace MyMemory.Tests
+{
+    [TestClass]
+    public class TestStudy : BaseTest
+    {
+        MemoryManager _mng;
+        StudyManager _mngStudy;
+
+        [TestInitialize]
+        public void Init()
+        {
+            MemoryDbContext.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=_test_MyMemory;Integrated Security=True";
+            _mng = new MemoryManager();
+            _mngStudy = new StudyManager();
+            DeleteItemsInDB();
+        }
+
+        [TestMethod]
+        public void TestCascades()
+        {
+            var mngItem = new ItemManager();
+            var item = mngItem.GetNextItem();
+            Assert.IsNull(item);
+
+        }
+    }
+}
