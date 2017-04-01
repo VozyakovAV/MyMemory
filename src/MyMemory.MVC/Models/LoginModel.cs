5fd50f6a3891d3183c7bb491377a5a8093caede8
andrew <andvoz@mail.ru>
2017-04-01 14:52:28 +0400

    fx view login

--- src/MyMemory.MVC/Models/LoginModel.cs
+++ src/MyMemory.MVC/Models/LoginModel.cs
@@ -9,25 +9,30 @@ namespace MyMemory.MVC.Models
     public class LoginModel
     {
         [Required]
+        [Display(Name = "Имя")]
         public string Name { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
     }
 
     public class RegisterModel
     {
         [Required]
+        [Display(Name = "Имя")]
         public string Name { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [Display(Name = "Пароль еще раз")]
         public string ConfirmPassword { get; set; }
     }
 }
