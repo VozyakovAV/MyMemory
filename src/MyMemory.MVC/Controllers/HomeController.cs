91209e713fd94ae4170bf12f6961c4edf59a0cbf
andrew <andvoz@mail.ru>
2017-04-28 09:47:49 +0400

    added studyController

--- src/MyMemory.MVC/Controllers/HomeController.cs
+++ src/MyMemory.MVC/Controllers/HomeController.cs
@@ -14,23 +14,13 @@ namespace MyMemory.MVC.Controllers
         private const string DATA_SESSION = "StudyData";
 
         private readonly MemoryManager _mng;
-        private readonly StudyManager _mngStudy;
 
         public HomeController()
         {
             _mng = new MemoryManager();
-            _mngStudy = new StudyManager()
-            {
-                IsRandom = true
-            };
         }
 
         public ActionResult Index()
-        {
-            return View("Study");
-        }
-
-        public ActionResult Study()
         {
             return View();
         }
@@ -39,32 +29,6 @@ namespace MyMemory.MVC.Controllers
         {
             ViewBag.NumberOfTasks = _mng.GetTasks().Length;
             return View();
-        }
-
-        public JsonResult StartStudy(int groupId)
-        {
-            var userName = this.User.Identity.Name;
-            var data = _mngStudy.Start(userName, groupId);
-            SaveStudyData(data);
-            return Json(data);
-        }
-
-        public JsonResult NextStep(string answer)
-        {
-            var data = LoadStudyData();
-            var data2 = _mngStudy.NextStep(data, answer);
-            SaveStudyData(data2);
-            return Json(data2);
-        }
-
-        private StudyData LoadStudyData()
-        {
-            return Session[DATA_SESSION] as StudyData;
-        }
-
-        private void SaveStudyData(StudyData data)
-        {
-            Session[DATA_SESSION] = data;
         }
     }
 }
---
+++ src/MyMemory.MVC/Controllers/StudyController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMemory.BLL;
+
+namespace MyMemory.MVC.Controllers
+{
+    public class StudyController : Controller
+    {
+        private const string DATA_SESSION = "StudyData";
+
+        private readonly MemoryManager _mng;
+        private readonly StudyManager _mngStudy;
+
+        public StudyController()
+        {
+            _mng = new MemoryManager();
+            _mngStudy = new StudyManager()
+            {
+                IsRandom = true
+            };
+        }
+
+        public ActionResult Index()
+        {
+            return View("Study");
+        }
+
+        public ActionResult Study()
+        {
+            return View();
+        }
+
+        public JsonResult StartStudy(int groupId)
+        {
+            var userName = this.User.Identity.Name;
+            var data = _mngStudy.Start(userName, groupId);
+            SaveStudyData(data);
+            return Json(data);
+        }
+
+        public JsonResult NextStep(string answer)
+        {
+            var data = LoadStudyData();
+            var data2 = _mngStudy.NextStep(data, answer);
+            SaveStudyData(data2);
+            return Json(data2);
+        }
+
+        private StudyData LoadStudyData()
+        {
+            return Session[DATA_SESSION] as StudyData;
+        }
+
+        private void SaveStudyData(StudyData data)
+        {
+            Session[DATA_SESSION] = data;
+        }
+    }
+}
